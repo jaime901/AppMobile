62606493c3fc0334e2bab9265af2374d1379046a
jaime901 <140458356+jaime901@users.noreply.github.com>
2023-07-29 11:39:51 -0500

    emision inicial api
    emision inicial api

---
+++ AppMovilAPI/AppMovilAPI/Data/AppMovilDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AppMovilAPI.Data.Models;
+
+namespace AppMovilAPI.Data
+{
+    public class AppMovilDbContext : DbContext
+    {
+        public AppMovilDbContext (DbContextOptions<AppMovilDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AppMovilAPI.Data.Models.Client> Client { get; set; } = default!;
+    }
+}
---
+++ AppMovilAPI/AppMovilAPI/Data/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppMovilAPI.Data.Models
+{
+    public class Client
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
